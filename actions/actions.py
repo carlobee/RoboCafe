25c66beafb280971974b7050dba215dd1a881f81
Morriss, Timothy <tm224@hw.ac.uk>
2021-03-01 10:19:31 +0000

    implemented clearing slots

--- actions/actions.py
+++ actions/actions.py
@@ -25,3 +25,22 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk import Action, Tracker
+
+from rasa_sdk.events import AllSlotsReset
+
+
+class ActionResetSlots(Action):
+
+    # defines the name of the custom action
+    def name(self) -> Text:
+        return "action_reset_slots"
+
+    # defines what happens when the custom action is run
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        return [AllSlotsReset()]
