244a53520868794e951e0132aa37f7a85d71f1a6
Morriss, Timothy <tm224@hw.ac.uk>
2021-03-10 16:15:35 +0000

    updated furhat interation.kt to opendomain speech

--- furhat/Skill/src/main/kotlin/furhatos/app/skill/flow/interaction.kt
+++ furhat/Skill/src/main/kotlin/furhatos/app/skill/flow/interaction.kt
@@ -6,6 +6,8 @@ import furhatos.nlu.Intent
 //import furhatos.app.skill.nlu.*
 import org.json.JSONObject
 
+var coffee_complete = 0
+
 val Start : State = state(Interaction) {
     onEntry {
         with(furhat) { listen() }
@@ -19,8 +21,22 @@ val Start : State = state(Interaction) {
     onResponse { response ->
         System.out.println(response.text)
         val ALANA_URL = "http://52.56.181.83:5000"
-        val alana_data = "{'user_id': 'test-user', 'question': "+  response.text + ", 'session_id': 'CLI-sessionId', 'projectId': 'CA2020', 'overrides': {'BOT_LIST':[{'greetings':'http://b572c08e8052.ngrok.io/webhooks/myio/webhook'}], 'PRIORITY_BOTS':['greetings']}}"
+//        val RASA_URL = "http://edcd47e520ab.ngrok.io/webhooks/myio/webhook"
+        val COMPLETE_STRING = "Be sure to release the handle again for the next person."
+//        val coffee_complete = false
+
+        var alana_data = " "
+        if (coffee_complete > 0) {
+            alana_data = "{'user_id': 'test-user', 'question': " +  response.text + ", 'session_id': '03.UUID4', 'projectId': 'CA2020', 'overrides': {'BOT_LIST':['weather_bot', 'wiki_bot_mongo', 'reddit_bot', {'greetings':'http://edcd47e520ab.ngrok.io/webhooks/myio/webhook'}], 'PRIORITY_BOTS':['greetings', 'weather_bot', 'reddit_bot', 'wiki_bot_mongo']}}"
+//            val response = khttp.post(ALANA_URL, data= JSONObject(alana_data)).text
+        } else {
+            alana_data = "{'user_id': 'test-user', 'question': " +  response.text + ", 'session_id': '03.UUID4', 'projectId': 'CA2020', 'overrides': {'BOT_LIST':[{'greetings':'http://edcd47e520ab.ngrok.io/webhooks/myio/webhook'}], 'PRIORITY_BOTS':['greetings']}}"
+//            val response = khttp.post(ALANA_URL, data= JSONObject(alana_data)).text
+        }
         val response = khttp.post(ALANA_URL, data= JSONObject(alana_data)).text
+        if(JSONObject(response).getString("result").endsWith(COMPLETE_STRING)) {
+            coffee_complete++
+        }
 //        System.out.println()
         furhat.say(JSONObject(response).getString("result"))
         with(furhat) { listen() }
